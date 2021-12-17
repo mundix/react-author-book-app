aa2b9ce6b9353500f79e1cfe47c853b96b41346d
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-12-16 23:16:08 -0400

    Implemented React Bootstrap

--- src/AuthorBookApp.js
+++ src/AuthorBookApp.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
 
 export const AuthorBookApp = () => {
 
-    const [state, setState] = useState({searchQuery: ''});
-    const {searchQuery} = state;
+    const [state, setState] = useState({ searchQuery: '' });
+    const { searchQuery } = state;
 
-    const handleInputChange = ({target}) => {
-        // console.log(target.value);
-        setState({searchQuery: target.value});
+    const handleInputChange = ({ target }) => {
+        setState({ searchQuery: target.value });
     }
 
 
     return (
         <>
-           <h1>Author Book App</h1> 
-           <div>
-               <input 
+            <h1>Author Book App</h1>
+            <div>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control 
                     type="text" 
-                    className="form-control" 
-                    name='searchQuery'
+                    placeholder="Search by Authors, Books..."
                     onChange={handleInputChange}
-                    />
-            <h2>{searchQuery}</h2>
+                     />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <h2>{searchQuery}</h2>
 
-           </div>
+            </div>
         </>
     )
 }
