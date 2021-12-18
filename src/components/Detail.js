0dcfe0cbce64f5575459935c6db641cb8ec110c5
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-12-17 23:02:50 -0400

    Add title and cover to Detail Component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 export const Detail = ({entry}) => {
+
+
+    const [coverUrl, setCoverUrl] = useState(null);
+
+    const getCoverUrl = (entry) => {
+        const cover = entry?.covers ? entry.covers[0] : null;
+        const url = `https://covers.openlibrary.org/b/id/${cover}-L.jpg`;
+        
+        if(cover) {
+            return url;
+        }
+        return null;
+    }
+
+    useEffect(()=>{
+        setCoverUrl(getCoverUrl(entry));
+    }, []);
+
+   
     return (
         <div>
             <h2>{entry.title}</h2>
+            <hr />
+            <div>
+                <img src={coverUrl} alt={coverUrl} />
+            </div>
         </div>
     )
 }
