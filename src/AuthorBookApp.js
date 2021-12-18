6cffb9d460722129d3f69c288b19a7260960bb80
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-12-17 23:45:35 -0400

    ...

--- src/AuthorBookApp.js
+++ src/AuthorBookApp.js
@@ -1,53 +1,70 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { AuthorContext } from './contexts/AuthorContext';
-import { AuthorDocs } from './components/AuthorDocs';
-import { DocDetails } from './components/DocDetails';
-import { EntryContext } from './contexts/EntryContext';
+import { Detail } from './components/Detail';
+import { Entry } from './components/Entry';
+import { WorkItem } from './components/WorkItem';
 
 export const AuthorBookApp = () => {
 
-    const [authorDocs, setAuthorDocs] = useState(null);
-    const [isAuthorDocs, setIsAuthorDocs] = useState(false);
-    const [authorDocEntries, setAuthorDocEntries] = useState(null);
+    const [querySearch, setQuerySearch] = useState(null);
+    const [authorWorks, setAuthorWorks] = useState(null);
+    const [isAuthorHasWorks, setIsAuthorHasWorks] = useState(false);
+    const [workEntries, setWorkEntries] = useState(null);
+    const [entry, setEntry] = useState(null);
 
-    const handleInputChange = async ({ target }) => {
-        setIsAuthorDocs(false);
-        if (!isAuthorDocs) {
 
-            await axios.get(`https://openlibrary.org/search/authors.json?q=${target.value}`)
-                .then(resp => {
-                    setAuthorDocs(resp.data.docs);
-                })
+    const handleInputChange = ({ target }) => {
+        setQuerySearch(target.value);
+    }
+
+    const handleKeyPress = async (e) => {
+        if (e.key === 'Enter') {
+
+            setIsAuthorHasWorks(false);
+            setWorkEntries(null);
+            setEntry(null);
+
+            if (!isAuthorHasWorks) {
+
+                await axios.get(`https://openlibrary.org/search/authors.json?q=${querySearch}`)
+                    .then(resp => {
+                        setAuthorWorks(resp.data.docs);
+                        setIsAuthorHasWorks(true);
+                    })
+            }
         }
     }
 
-    
 
     return (
-        <AuthorContext.Provider value={setIsAuthorDocs, setAuthorDocEntries}>
-            <div className='row col-10 d-flex p-5'>
-                <div className="form-group ">
-                    <input
-                        className='form-control p-4 justify-center'
-                        type="text"
-                        className="form-control"
-                        onChange={handleInputChange}
-                        placeholder='Type Author or Book name ... '
-                    />
-                </div>
-                {
-                    !!authorDocs && !isAuthorDocs && (
-                        <AuthorDocs docs={authorDocs} />
-                    )
-                }
-                {
-                    isAuthorDocs && (
-                        <DocDetails authorDocEntries={authorDocEntries}/>
-                    )
-                }
-
+        <div className='row col-10 d-flex p-5'>
+            <div className="form-group ">
+                <input
+                    className='form-control p-4 justify-center'
+                    type="text"
+                    className="form-control"
+                    onChange={handleInputChange}
+                    onKeyPress={handleKeyPress}
+                    placeholder='Type Author or Book name ... '
+                />
+                <small>Search for: {querySearch}</small>
             </div>
-        </AuthorContext.Provider>
+            <div>
+                {
+                    entry === null
+                        ? (
+                            <ul className='mt-3'>
+                                {
+                                    !!authorWorks && workEntries === null && authorWorks.map(work => <WorkItem key={'work_' + work.key} work={work} setWorkEntries={setWorkEntries} setQuerySearch={setQuerySearch} />)
+                                }
+                                {
+                                    !!workEntries && workEntries.map(entry => <Entry key={'entry_' + entry.key} entry={entry} setEntry={setEntry} setQuerySearch={setQuerySearch} />)
+                                }
+                            </ul>
+                        )
+                        : (<Detail entry={entry}/>)
+                }
+            </div>
+        </div>
     )
 }
